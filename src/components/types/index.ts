85f85d70d5701e070294b72f78ffa061a343e90c
Shivam Shukla <shivam@gkmit.co>
2023-02-11 23:33:17 +0530

    created layout

--- src/components/types/index.ts
+++ src/components/types/index.ts
@@ -11,3 +11,21 @@ export interface SkillInterface {
   level: string;
   Icon: IconType;
 }
+
+export interface ProjectInterface {
+  name: string;
+  description: string;
+  image_path: string;
+  deployed_url: string;
+  github_url: string;
+  category: Category[];
+  key_techs: string[];
+}
+
+export type Category =
+  | 'reactjs'
+  | 'nextjs'
+  | 'nodejs'
+  | 'mysql'
+  | 'javascript'
+  | 'firebase';
